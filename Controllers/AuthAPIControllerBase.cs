03b74e6064c2f4f5594a70f1ea861a584db6fda5
Mahdi Rostami <MahdiRostami@Outlook.de>
2021-12-05 20:09:35 +0300

    Add Authentication controller Add base controller for Authorize required Controllers

---
+++ Controllers/AuthAPIControllerBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ToDoAPI.Extensions;
+
+namespace API.Controllers
+{
+
+
+    [Authorize]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthAPIControllerBase : ControllerBase
+    {
+
+        public ToDoAPI.Models.User CurrentUser => User.GetUserInfoFromClaims();
+        public AuthAPIControllerBase()
+        {
+
+        }
+
+    }
+}
---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ToDoAPI.Models;
+using ToDoAPI.Services;
+using ToDoAPI.Uilities.Responses;
+
+namespace ToDoAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService authenticationService;
+
+        public AuthenticationController(IAuthenticationService service)
+        {
+            this.authenticationService = service;
+        }
+
+
+        [HttpGet]
+        public ActionResult IsAuthenticated()
+        {
+            return Ok(User.Identity.IsAuthenticated);
+        }
+        [HttpGet]
+        public ActionResult AuthenticatedUser()
+        {
+
+            return Ok(User.Identity.Name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(ProfileCreateRequestCommand profileCreateRequestCommand)
+        {
+
+            BaseResponse<User> response = await authenticationService.SignUp(profileCreateRequestCommand);
+
+
+            if (response.Success)
+            {
+                return Ok(response.Extra);
+            }
+            else
+            {
+                return BadRequest(response.Message);
+            }
+
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(LoginCommmand signInViewModel)
+        {
+
+            var response = await authenticationService.SignIn(signInViewModel);
+
+            if (response.Success)
+            {
+                return Ok(response.Extra);
+            }
+            else
+            {
+                return BadRequest(response.Message);
+            }
+
+
+        }
+
+
+
+
+    }
+}
